604acbc6d543087c69586d366a903dd5ead0c9ab
aryn weatherly <arynweatherly@aryns-MacBook-Pro.local>
2019-10-08 15:24:25 -0500

    did some basic styling and navbar

---
+++ src/home/home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+// import './Home.css'
+class Home extends Component {
+
+    render() {
+        return (
+            <div >
+                <h2>HEXBook Homepage<br />
+                    <small>home page slogan</small>
+                </h2>
+            </div >
+        )
+    }
+}
+
+export default Home;
---
+++ src/scripts/applicationViews/applicationviews.js
@@ -0,0 +1,19 @@
+import { Route, Redirect } from 'react-router-dom'
+import React, { Component } from 'react'
+import Home from '../../home/home'
+import Login from '../login/login'
+export default class ApplicationViews extends Component {
+
+    // isAuthenticated = () => localStorage.getItem("credentials") !== null
+
+render() {
+    return (
+      <React.Fragment>
+        <Route exact path="/" render={(props) => {
+          return <Home />
+        }} />
+
+            <Route path="/login" render={props => {
+          return <Login setUser={this.props.setUser} {...props} />
+        }} />
+    </React.Fragment> ) }}

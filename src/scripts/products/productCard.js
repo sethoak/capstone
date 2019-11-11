c641a84c7972f3f98db34a9dca88dde1985c88fe
sethoak <sethoak@gmail.com>
2019-11-10 20:25:32 -0600

    saving code for face selection

--- src/scripts/products/productCard.js
+++ src/scripts/products/productCard.js
@@ -98,7 +98,7 @@ export default class ProductCard extends Component {
                 TRY THEM ON{" "}
                 <img
                   src={Sunglasses}
-                  onClick={this.props.setGlassesURLCB.bind(this.props.product)}
+                  onClick={this.props.setGlassesURLCB.bind(this.props.product.sunglassesImageSelection)}
                 />
               </p>
               {/*ENDS try div*/}
